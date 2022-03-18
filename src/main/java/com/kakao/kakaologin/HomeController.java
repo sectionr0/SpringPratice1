5389b9f4f900fe58eed163bab7ef4073d3a6eaea
sectionr0 <sectionr0@gmail.com>
2022-03-18 16:53:45 +0900

    Create HomeController.java
    컨트롤러 생성

---
+++ src/main/java/com/kakao/kakaologin/HomeController.java
@@ -0,0 +1,36 @@
+package com.kakao.kakaologin;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpSession;
+import java.util.HashMap;
+
+@RestController
+public class HomeController {
+
+    KakaoAPI kakaoAPI = new KakaoAPI();
+
+    @RequestMapping(value = "/login")
+    public ModelAndView login(@RequestParam("code") String code, HttpSession session) {
+        ModelAndView mav = new ModelAndView();
+
+        // 1. 인증 코드 요청 전달
+        String accessToken = kakaoAPI.getAccessToken(code);
+        // 2. 인증 코드로 토큰 전달
+        HashMap<String, Object> userInfo = kakaoAPI.getUserInfo(accessToken);
+
+        System.out.println("userInfo = " + userInfo.toString());
+
+        if (userInfo.get("email") != null) {
+            session.setAttribute("userId", userInfo.get("email"));
+            session.setAttribute("access_token", accessToken);
+        }
+
+        mav.addObject("userId", userInfo.get("email"));
+        mav.setViewName("index");
+        return mav;
+    }
+}
